7af6ee04eaee719cf2de63d0ccabde0501e0621b
Evgeny Kuznetsov <eugenschmidt@yandex-team.ru>
2025-05-26 22:54:56 +0300

    increment 20

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,8 +1,22 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/nekr0z/muhame/internal/agent"
 )
+
+var (
+	buildVersion = "N/A"
+	buildDate    = "N/A"
+	buildCommit  = "N/A"
+)
+
+func init() {
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
+	fmt.Printf("Build commit: %s\n", buildCommit)
+}
 
 func main() {
 	agent.New().Run()
